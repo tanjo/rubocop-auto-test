c772f31cfa749f335375664ead8c564ffea0a691
tanjo <tanjo@users.noreply.github.com>
2026-02-27 11:27:59 +0900

    add unformat code

---
+++ src/test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+
+$x = 0
+
+class testclass < Minitest::Test
+	def testSomething; a = [1,2,3]; assert_equal(3,a.size); if a.size > 0 then $x = $x + 1 else $x = 0 end; assert($x > 0); end
+
+	def test_another_thing
+		foo = "abc"
+		assert_equal("abc", foo)
+	end
+end
