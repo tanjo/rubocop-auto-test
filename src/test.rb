88d7cdab18a9fcbd87ef6e82e478b96d65fb1f8c
tanjo <tanjo@users.noreply.github.com>
2026-02-27 11:36:11 +0900

    エラーが酷すぎたので修正

--- src/test.rb
+++ src/test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 
 $x = 0
 
-class testclass < Minitest::Test
+class TestClass < Minitest::Test
 	def testSomething; a = [1,2,3]; assert_equal(3,a.size); if a.size > 0 then $x = $x + 1 else $x = 0 end; assert($x > 0); end
 
 	def test_another_thing
