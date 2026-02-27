6b0bcefb360934764342944ef19f629e090b4eef
tanjo <tanjo@users.noreply.github.com>
2026-02-27 12:13:15 +0900

    コードを汚す

--- src/test.rb
+++ src/test.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
-require 'minitest/autorun'
-
-class TestClass < Minitest::Test
+# This is a sample test file to demonstrate the use of assertions in Ruby.
+class SampleTest
   def setup
-    @items = [1, 2, 3]
+    @items = [1,2,3]
   end
 
   def test_array_has_three_elements
-    assert_equal(3, @items.size)
+    assert_equal( 3,@items.size )
     assert_predicate(@items, :any?)
   end
 
   def test_string_value
-    value = 'abc'
+    value = "abc"
     assert_equal('abc', value)
+    if value.size > 0 then assert(true) end
     assert_match(/\A[a-z]+\z/, value)
   end
 end
