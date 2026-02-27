3930ab2a7ff15c5829bbd793f2b8ea9e024c5a8b
Yuuki Tanjo <tanjo@users.noreply.github.com>
2026-02-27 11:59:27 +0900

    Update src/test.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/test.rb
+++ src/test.rb
@@ -3,7 +3,12 @@ require 'minitest/autorun'
 $x = 0
 
 class TestClass < Minitest::Test
-	def testSomething; a = [1,2,3]; assert_equal(3,a.size); if a.size > 0 then $x = $x + 1 else $x = 0 end; assert($x > 0); end
+  def testSomething
+    a = [1, 2, 3]
+    assert_equal(3, a.size)
+    $x = a.size > 0 ? $x + 1 : 0
+    assert($x > 0)
+  end
 
 
   
